f4f0d58d7104b4fa52456b0798b743ed93e7c5a3
Jess Martin <jessicaprince360@gmail.com>
2021-07-21 22:03:38 -0700

    Added instructor class

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Instructor
+    {
+        /// <summary>
+        /// Legal full name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Company email address
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
